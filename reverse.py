102a1b4e4e1c6e43ffc42acce32f6d467bbbf9a0
Govind Gupta <govind.15986@stu.upes.ac.in>
2024-09-08 22:43:53 +0530

    python reverse code

---
+++ reverse.py
@@ -0,0 +1,8 @@
+# Take user input
+user_input = input("Please enter a string: ")
+
+# Reverse the string using indexing
+reversed_string = user_input[::-1]
+
+# Display the reversed string
+print("Reversed string:", reversed_string)
